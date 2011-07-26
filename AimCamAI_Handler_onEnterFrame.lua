3e21143356761b6ab226bf742c545e7a8b373b4c
Wade Tracy <wade.tracy@subspacegames.com>
2011-07-25 23:00:54 -0700

    Vid 36 - Quick addition of target and level completion

--- AimCamAI_Handler_onEnterFrame.lua
+++ AimCamAI_Handler_onEnterFrame.lua
@@ -11,32 +11,7 @@ function AimCamAI.onEnterFrame (  )
 
     -- Check for the end of the turn
 	if( this.turnEnds ( ) ) then
-        this.ballInPlay ( false )
-        
-        -- Reset the ball
-        object.sendEvent ( this.hBall ( ), "BallAI", "onReset", this.nStartX ( ),
-                           this.nStartY ( ), this.nStartZ ( ) )
-        
-        -- Reset the fire
-        local hFire = application.getCurrentUserSceneTaggedObject ( "FireHelper" )
-        object.sendEvent ( hFire, "FireAI", "onReset" )
-        
-        -- Reset the chase cam
-        object.sendEvent ( this.hChaseCam ( ), "ChaseCamAI", "onReset", this.nStartX ( ),
-                           this.nStartY ( ), this.nStartZ ( ) )
-        
-        -- Change the camera, the cursor visibility, and the location of the ceiling
-        -- based on whether it is in overhead mode
-        if( not this.bOverhead ( ) ) then
-            application.setCurrentUserActiveCamera ( this.hAimCam ( ) )
-            this.setCrosshairVisibility ( true )
-        else
-            -- Set us up for editing by showing the cursor and moving the ceiling
-			local hUser = application.getCurrentUser ( )
-            hud.setCursorVisible ( hUser, true )
-            object.setTranslation ( this.hCeiling ( ), 0, -this.nCeilingAdjustment ( ),
-                                    0, object.kLocalSpace)
-        end
+        object.sendEvent ( this.hAimCam ( ), "AimCamAI", "onTurnEnds" )
     end
 	
 --------------------------------------------------------------------------------
---
+++ AimCamAI_Handler_onTurnEnds.lua
@@ -0,0 +1,40 @@
+--------------------------------------------------------------------------------
+--  Handler.......... : onTurnEnds
+--  Author........... : 
+--  Description...... : 
+--------------------------------------------------------------------------------
+
+--------------------------------------------------------------------------------
+function AimCamAI.onTurnEnds (  )
+--------------------------------------------------------------------------------
+	
+    this.ballInPlay ( false )
+    
+	-- Reset the ball
+    object.sendEvent ( this.hBall ( ), "BallAI", "onReset", this.nStartX ( ),
+                       this.nStartY ( ), this.nStartZ ( ) )
+    
+    -- Reset the fire
+    local hFire = application.getCurrentUserSceneTaggedObject ( "FireHelper" )
+    object.sendEvent ( hFire, "FireAI", "onReset" )
+    
+    -- Reset the chase cam
+    object.sendEvent ( this.hChaseCam ( ), "ChaseCamAI", "onReset", this.nStartX ( ),
+                       this.nStartY ( ), this.nStartZ ( ) )
+    
+    -- Change the camera, the cursor visibility, and the location of the ceiling
+    -- based on whether it is in overhead mode
+    if( not this.bOverhead ( ) ) then
+        application.setCurrentUserActiveCamera ( this.hAimCam ( ) )
+        this.setCrosshairVisibility ( true )
+    else
+        -- Set us up for editing by showing the cursor and moving the ceiling
+        local hUser = application.getCurrentUser ( )
+        hud.setCursorVisible ( hUser, true )
+        object.setTranslation ( this.hCeiling ( ), 0, -this.nCeilingAdjustment ( ),
+                                0, object.kLocalSpace)
+    end
+	
+--------------------------------------------------------------------------------
+end
+--------------------------------------------------------------------------------
